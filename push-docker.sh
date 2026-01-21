69dad95c23cd95fe203d8698ada2aed7d40358c0
danakj <danakj@chromium.org>
2026-01-21 16:17:04 -0500

    support different docker tags than 'latest'

--- push-docker.sh
+++ push-docker.sh
@@ -2,9 +2,11 @@
 
 usage() {
   echo "Usage: push-docker.sh [DOCKERHUBUSER]"
-  echo
-  echo "  Example usage: ./push-docker.sh danakj"
-  echo
+  echo ""
+  echo "  Example usage: ./push-docker.sh danakj [TAG]"
+  echo ""
+  echo "  TAG    The tag to use when pushing. Default: 'latest'."
+  echo ""
   echo "  Ensure you are logged into docker hub with that user via 'docker login'"
   exit 1
 }
@@ -14,6 +16,11 @@ if [[ "$USER" == "" ]]; then
   usage
 fi
 
-sudo docker tag fizz-bot:latest $USER/fizz-bot:latest || exit 1
-sudo docker push $USER/fizz-bot:latest
+TAG=$2
+if [[ "$TAG" == "" ]]; then
+  TAG=latest
+fi
+
+sudo docker tag fizz-bot:latest $USER/fizz-bot:$TAG || exit 1
+sudo docker push $USER/fizz-bot:$TAG
 
